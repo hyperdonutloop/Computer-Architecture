7fd8a63a8abe8f6039f02d1b508e062ed60245ab
donutwizard666 <thebeardedwarrior@icloud.com>
2020-05-20 20:13:11 -0700

    added in some changes

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,12 @@
 
 import sys
 
+PUSH = 0b01000101
+POP = 0b01000110
+
+SP = 7
+STACK_START = 0xF4
+
 class CPU:
     """Main CPU class."""
 
@@ -15,35 +21,56 @@ class CPU:
         self.HLT = 0b00000001
         self.LDI = 0b10000010
         self.PRN = 0b01000111
+        self.MUL = 0b10100010
+        self.register[SP] = STACK_START
 
     def load(self):
         """Load a program into memory."""
+        filename = './examples/print8.ls8'
+        
+        if len(sys.argv) == 2:
+            filename = sys.argv[1]
 
         address = 0
+        
 
-        # For now, we've just hardcoded a program:
+        with open(filename) as program:
+            for ins in program:
+                ins = ins.split('#')
+                ins = ins[0].strip()
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                if ins == '':
+                    continue
+                self.ram[address] = int(ins, 2)
+                address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+
+        # # For now, we've just hardcoded a program:
+
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.register[reg_a] += self.register[reg_b]
+        
+        elif op == 'MUL':
+            self.register[reg_a] *= self.register[reg_b]
+        
         else:
             raise Exception("Unsupported ALU operation")
     
@@ -56,9 +83,9 @@ class CPU:
         # RR should accept the address to read and return the value
         return self.ram[address]
     
-    def ram_write(self, address, payload):
+    def ram_write(self, address, value):
         # RW should accept a value to write and the address to write it to
-        self.ram[address] = payload
+        self.ram[address] = value
 
     def trace(self):
         """
@@ -83,7 +110,9 @@ class CPU:
     def run(self):
         """Run the CPU."""
         # STEP 3 - Implement the core of RUN
+        # wherever the pc is, this is storing it into the Instruction Register
         IR = self.pc
+        
 
         while self.running:
             instruction = self.ram[IR]
@@ -101,6 +130,27 @@ class CPU:
                 value = self.register[reg_num]
                 print(value)
                 IR += 2
+            
+            elif instruction == self.MUL:
+                self.alu('MUL', self.ram[IR + 1], self.ram[IR + 2])
+                IR += 3
+            
+            elif instruction == PUSH:
+                # decrement the SP
+                self.register[SP] -= 1
+                # get resgister number
+                reg_num = self.ram[IR + 1]
+                self.ram[self.register[SP]] = self.register[reg_num]
+                self.pc += 2
+            
+            elif instruction == POP:
+                reg_num = self.ram_read(self.pc+1)
+
+                # stack_address = self.register[SP]
+                # stack_value = self.ram[stack_address]
+                # self.register[reg_num] = stack_value
+                self.register[SP] += 1
+                self.pc +=2
 
             elif instruction == self.HLT:
                 # HALTS THE CPU AND EXIT THE EMULATOR
@@ -109,7 +159,3 @@ class CPU:
             else:
                 print ('Invalid Instructions')
                 self.halt()
-
-
-        
-        pass
